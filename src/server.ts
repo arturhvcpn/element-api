b0429354b8f9788ef7f9598e3322747e4aecfe6d
Artur Polo Norte <arturpolonorte@MacBook-Air-de-Artur.local>
2022-03-26 01:13:44 -0300

    :memo: create swagger

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,15 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import { router } from './routes';
 import { logger } from './utils/Logger';
+import { serve, setup } from 'swagger-ui-express';
+import swaggerJson from '../swagger.json';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/api/swagger', serve, setup(swaggerJson));
 app.use(router);
 
 app.listen(process.env.PORT,() => {
